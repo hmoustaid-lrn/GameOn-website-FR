a8c2aacad28938b95a34c96dc6668bbe3821afb9
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-02-03 17:23:18 +0100

    Ajouter conformation quand envoi réussi fixed https://github.com/OpenClassrooms-Student-Center/GameOn-website-FR/issues/4#issue-672221607

--- modal.js
+++ modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector(".btn-submit");
+const formBody = document.getElementsByName("reserve"); 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +22,34 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Confirmation après envoi réussi   
+function displayThanks(){
 
+  formBody[0].style.display = "none";
+  const modalBody = document.getElementsByClassName("modal-body");
+  const confirmMsg = document.createElement("div");
+  const confirmBtn = document.createElement("div");
+  modalBody[0].appendChild(confirmMsg);
+  modalBody[0].appendChild(confirmBtn);
+  confirmMsg.setAttribute("class", "confirmation");
+  confirmMsg.innerHTML = "Merci pour <br> votre inscription";
+  confirmBtn.className = "btn-close";
+  confirmBtn.innerHTML = "Fermer";
+  confirmBtn.addEventListener("click", function(){
+    validate();
+  });
+
+}
+
+
+// Evenement qui va afficher le message de confirmation après avoir rempli le formulaire
+submitBtn.addEventListener("click", function(event){
+  event.preventDefault();
+  displayThanks();
+});
+
+//Fermer le formulaire
+function validate(){
+  formBody[0].submit();
+  return true;
+}
